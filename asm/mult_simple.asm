0b01d41740db0384e599a9dc614a1eb21855c96c
David Michel <david.michelt@iteso.mx>
2025-09-14 14:00:48 -0600

    added simple asm program and updated rom memory to execute it

---
+++ asm/mult_simple.asm
@@ -0,0 +1,22 @@
+    .data
+    .text
+    .globl main 
+    
+main:
+
+addi t1, zero, 10
+addi t2, zero, 5
+addi t3, zero, 6
+addi t4, zero, 3
+addi t5, zero, 8
+mul t6, t1, t2
+addi t1, zero, 13
+addi t2, zero, 23
+addi t3, zero, 34
+addi t4, zero, 54
+addi t5, zero, 86
+addi t1, zero, 12
+addi t2, zero, 54
+addi t3, zero, 22
+addi t4, zero, 33
+addi t5, zero, 44
